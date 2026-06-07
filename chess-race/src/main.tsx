2898dcbc12429766bfcfdc740f6d9bf1b5d8eb62
Farhaj Shahid <sfs016@bucknell.edu>
2026-06-06 23:02:26 -0400

    Add Vercel Analytics
    Install @vercel/analytics and render <Analytics/> in main.tsx to collect
    page views on the deployed site.

--- chess-race/src/main.tsx
+++ chess-race/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App.tsx";
 import { Identity } from "spacetimedb";
@@ -39,5 +40,6 @@ createRoot(document.getElementById("root")!).render(
     <SpacetimeDBProvider connectionBuilder={connectionBuilder}>
       <App />
     </SpacetimeDBProvider>
+    <Analytics />
   </StrictMode>,
 );
